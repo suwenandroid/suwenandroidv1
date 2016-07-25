3f88d1a2a21e0e0c121ba78830759afd5bec7de5
chanzhao <pizicz@163.com>
2016-07-25 21:54:27 +0800

    项目的构建测试包-chanzhao

---
+++ app/src/main/java/com/suwen/suwenandroid2016v1/ui/Activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.suwen.suwenandroid2016v1.ui.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/7/25.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
