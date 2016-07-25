ca83109674408500cc16211067654c35db5cca45
niuhongbin <niuhongbin@wepiao.com>
2016-07-25 22:38:12 +0800

    [+]增加了搜索和mine碎片

---
+++ app/src/main/java/com/suwen/suwenandroid2016v1/ui/Fragment/SearchFragment.java
@@ -0,0 +1,49 @@
+package com.suwen.suwenandroid2016v1.ui.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.suwen.suwenandroid2016v1.R;
+
+
+public class SearchFragment extends Fragment {
+
+
+    public SearchFragment() {}
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_su_wen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+
+}
