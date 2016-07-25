581279cab2ef4b1c48935cefc84a6322d4943c9c
chanzhao <pizicz@163.com>
2016-07-25 21:51:19 +0800

    项目的构建测试包-chanzhao

--- app/src/main/java/com/suwen/suwenandroid2016v1/ui/Activity/MainActivity.java
+++ app/src/main/java/com/suwen/suwenandroid2016v1/ui/Activity/MainActivity.java
@@ -1,15 +1,15 @@
-package com.suwen.suwenandroid2016v1;
+package com.suwen.suwenandroid2016v1.ui.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.suwen.suwenandroid2016v1.R;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Hello!!!",Toast.LENGTH_SHORT).show();
     }
 }
