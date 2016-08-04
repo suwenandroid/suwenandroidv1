74e26290cfe88c73572e42ee107afcf1bf2e7b2d
chanzhao <pizicz@163.com>
2016-08-04 13:28:04 +0800

    [*]添加了humble

--- app/src/main/java/com/suwen/suwenandroid2016v1/activity/MainActivity.java
+++ app/src/main/java/com/suwen/suwenandroid2016v1/activity/MainActivity.java
@@ -1,33 +1,44 @@
 package com.suwen.suwenandroid2016v1.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.ListPopupWindow;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.suwen.suwenandroid2016v1.R;
+import com.suwen.suwenandroid2016v1.adapter.MainListPopAdapter;
 import com.suwen.suwenandroid2016v1.fragment.MineFragment;
 import com.suwen.suwenandroid2016v1.fragment.SearchFragment;
 import com.suwen.suwenandroid2016v1.fragment.SuWenChildFragment;
+import com.suwen.suwenandroid2016v1.log.MyLog;
 import com.suwen.suwenandroid2016v1.utils.BottomTabUtils;
 import com.suwen.suwenandroid2016v1.utils.ToastUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends BaseActivity implements View.OnClickListener,BottomTabUtils.BottomTabCallback {
+public class MainActivity extends BaseActivity implements View.OnClickListener, BottomTabUtils.BottomTabCallback {
+    public static final String TAG = "MainActivity";
+    private Context mContext;
     /**
      * 底部Tab RadioGroup
      */
-    private RadioGroup mBottomTabGroup;
-    private List<Fragment> mFragments;
+
     //title上面的字
     private TextView mTvTitle;
     //title上面的汉堡包
     private CheckBox mCbHumble;
     private BottomTabUtils bottomTabUtils;
+    private ListPopupWindow mListPopWindow;
+    private MainListPopAdapter mPopAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,23 +57,35 @@ public class MainActivity extends BaseActivity implements View.OnClickListener,B
 
     @Override
     protected void initView() {
-        setBottomTab(); //处理底部的Tab
         mTvTitle = (TextView) this.findViewById(R.id.tv_activit_title);
         mCbHumble = (CheckBox) this.findViewById(R.id.checkbox_hamble);
+        mPopAdapter = new MainListPopAdapter(getApplication(),R.layout.item_main_listpopwindow,Arrays.asList(getResources().getStringArray(R.array.array_main_humble)));
+        mListPopWindow = new ListPopupWindow(getApplication());
+        mListPopWindow.setAdapter(mPopAdapter);
+        mListPopWindow.setWidth(120);
+        mListPopWindow.setHeight(ListPopupWindow.WRAP_CONTENT);
+        mListPopWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ToastUtils.show("position:"+position);
+            }
+        });
+
+
     }
 
     //设置main底部的Tab
     private void setBottomTab() {
-        mBottomTabGroup = (RadioGroup) this.findViewById(R.id.group_main_tab);
-        mFragments = new ArrayList<>();
+        List<Fragment> mFragments = new ArrayList<>();
         mFragments.add(SuWenChildFragment.getInstance());
         mFragments.add(SearchFragment.newInstance("", ""));
         mFragments.add(MineFragment.newInstance("", ""));
-        bottomTabUtils = new BottomTabUtils(mBottomTabGroup, mFragments, getSupportFragmentManager(), R.id.mian_container,this);
+        bottomTabUtils = new BottomTabUtils((RadioGroup) this.findViewById(R.id.group_main_tab), mFragments, getSupportFragmentManager(), R.id.mian_container, this);
     }
 
     @Override
     protected void initData() {
+        setBottomTab(); //处理底部的Tab
     }
 
     @Override
@@ -87,24 +110,49 @@ public class MainActivity extends BaseActivity implements View.OnClickListener,B
     }
 
     private void showPopWindow() {
-        if(mCbHumble.isChecked()) {
+        if (mCbHumble.isChecked()) {
             //显示popWindow
-            ToastUtils.show(mCbHumble.isChecked()+"");
+            ToastUtils.show(mCbHumble.isChecked() + "");
+            //指定anchor
+            mListPopWindow.setAnchorView(mCbHumble);
+            mListPopWindow.show();
         } else {
             //隐藏popWindow
-            ToastUtils.show(mCbHumble.isChecked()+"");
+            if(mListPopWindow != null) {
+                mListPopWindow.dismiss();
+            }
+            ToastUtils.show(mCbHumble.isChecked() + "");
         }
     }
 
     @Override
     public void BottomTabCurIndex(int index) {
-        ToastUtils.show(index+"");
+        setTitleText(index);
+
+    }
+
+    private void setTitleText(int index) {
+        String[] stringArray = getResources().getStringArray(R.array.array_title_name);
+        if (stringArray != null) {
+            String str = stringArray[index];
+            if (!TextUtils.isEmpty(str)) {
+                mTvTitle.setText(str);
+            }
+        } else {
+            MyLog.d(TAG, "get main title text is null");
+        }
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        bottomTabUtils.unRegistBottomTabCallback();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bottomTabUtils.registBottomTabCallback(this);
+    }
 }
